7f0cc54f8fe9a63ef7f523e9cf1a9ca5d75e6337
Foggyrocket <dyl.blink.1.8.2@gmail.com>
2018-08-17 17:05:11 -0500

    sumar restar sin redux done

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    state={
+        contador:0
+    }
+
+    sumar=()=>{
+        let {contador}=this.state;
+        contador += 1;
+        this.setState({contador})
+    }
+    restar=()=>{
+        let {contador}= this.state;
+
+        if(contador === 0){
+            alert("No hay nada que restar")
+        }
+        else{
+            contador -= 1;
+        }
+        this.setState({contador})
+
+    }
+
   render() {
+        let {contador}=this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" style={{height:400,display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+        <div style={{marginBottom:10}}>
+            <button onClick={this.sumar} style={{marginRight:5}}>Sumar</button>
+            <button onClick={this.restar}>Restar</button>
+        </div>
+
+          <text>Contador: {contador}</text>
+
       </div>
     );
   }
